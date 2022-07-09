ad3143d4d032e17b17e03ec379a003826ec8fd3d
ONKAR WAGH <86963691+onkarrw@users.noreply.github.com>
2022-07-09 18:16:53 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -3,9 +3,40 @@ let myleads =[]
 const ulel = document.getElementById("uleel")
 const input = document.getElementById("takeinput")
 const savebtn = document.getElementById('save-btn')
+const delbtn = document.getElementById("delete-btn")
+const tabbtn = document.getElementById("tab-btn")
+
+let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myleads"))
+
+if(leadsFromLocalStorage)
+{
+    myleads = leadsFromLocalStorage
+    renderleads()
+}
+
+console.log(leadsFromLocalStorage)
 
 
-console.log(localStorage.getItem("myleads"))
+
+tabbtn.addEventListener("click",function()
+{
+
+    chrome.tabs.query({active:true, currentWindow:true}, function(tabs){
+        myleads.push(tabs[0].url)
+        localStorage.setItem("myleads",JSON.stringify(myleads))
+        renderleads()
+    })
+
+})
+
+delbtn.addEventListener("dblclick",function (){
+    console.log("double clicked")
+   localStorage.clear()
+   myleads=[]
+   renderleads()
+
+})
+
 
 savebtn.addEventListener("click", function(){
 
@@ -13,16 +44,13 @@ savebtn.addEventListener("click", function(){
         // ulel.innerHTML += "<a>" + myleads + "</a>"
 
         input.value=""
+        localStorage.setItem("myleads", JSON.stringify(myleads))
+
         renderleads()
 
-        // const li = document.createElement("li")
-        // li.textContent = myleads
-        // ulel.append(li)
-
-
+        console.log(myleads)
+       
 })
-
-
 
 function renderleads()
 {
@@ -31,7 +59,6 @@ function renderleads()
     {
         // listitems +=  "<li><a href=" +  myleads[i] +">"+myleads[i] +"</a></li>"
 
-
         listitems += `
         <li>
             <a href ='${myleads[i]}'>
@@ -39,15 +66,32 @@ function renderleads()
                 ${myleads[i]}
 
             </a>
-        </li>`
-
-
-
-      
+        </li>`      
     }
-
     
     ulel. innerHTML = listitems
-    
-
 }
+
+
+ // const li = document.createElement("li")
+        // li.textContent = myleads
+        // ulel.append(li)
+
+
+// localStorage.setItem("myleads","www.example.com")
+// localStorage.getItem("myleads")
+// localStorage.clear()-
+
+// myleads  = JSON.stringify(myleads)
+
+// console.log(typeof(myleads))
+// myleads = JSON.parse(myleads)
+
+ 
+// console.log(typeof(myleads))
+// myleads.push("secondexample.com")
+// myleads= JSON.stringify(myleads)
+// console.log(typeof(myleads))
+
+
+// console.log(localStorage.getItem("myleads"))
